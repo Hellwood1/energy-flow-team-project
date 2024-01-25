8ddd5f29017d18251440ea146264f8f9baabc949
Vladyslav Sahaidak <svg.231@gmail.com>
2024-01-25 20:20:57 +0100

    doneHTML+start-Js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
+<<<<<<< Updated upstream
 "use strict";
+=======
+'use strict';
+
+import { closeModal } from './js/modal';
+import { mobileMenu } from './js/header-modal';
+
+mobileMenu()
+
+>>>>>>> Stashed changes
