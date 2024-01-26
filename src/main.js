1b9f9ce64fb04eabe223b57e49b54369c8932e8c
Vladyslav Sahaidak <svg.231@gmail.com>
2024-01-26 22:41:46 +0100

    fixedAndAddedNav

--- src/main.js
+++ src/main.js
@@ -1,11 +1,4 @@
-<<<<<<< Updated upstream
-"use strict";
-=======
-'use strict';
 
-import { closeModal } from './js/modal';
 import { mobileMenu } from './js/header-modal';
 
 mobileMenu()
-
->>>>>>> Stashed changes
