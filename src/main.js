0e616ecf3a96cbd30e3f8069ce8f64ff78bc7dee
Hellwood1 <samburik123@gmail.com>
2024-01-27 22:37:01 +0100

    Upd. main.js

--- src/main.js
+++ src/main.js
@@ -3,9 +3,10 @@
 import { closeModal } from './js/modal';
 import { addListeners } from './js/renderCategoriesByFilter';
 import { checkDate } from './js/quote';
-import './js/exercise-card';
 import { initRatings, manageRatingModal } from './js/giveRating';
+import { mobileMenu } from './js/header-modal';
 
+mobileMenu()
 addListeners();
 closeModal();
 checkDate();
