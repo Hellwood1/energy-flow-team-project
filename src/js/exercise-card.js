9df8f8b44d29eb6a448e6046de528ae9ae2d8f10
RomaShekel <141637507+RomaShekel@users.noreply.github.com>
2024-01-27 23:15:50 +0200

    стилі і пофікшені баги стосовно карток з вправами

--- src/js/exercise-card.js
+++ src/js/exercise-card.js
@@ -1,20 +1,19 @@
+
 import axios from 'axios';
 import EnergyFlowApiSevice from './api-service';
 
-const exercisesCardList = document.querySelector('.exercises-list');
-export const exerciseCardNameWidth = document.querySelector(
-  '.card-exercise-name'
-);
+const exercisesCardList = document.querySelector(".favorites-list");
+const exerciseCardNameWidth = document.querySelector(".card-exercise-name")
+const favoriteDivWithoutCards = document.querySelector(".favorites-div-without-cards")
+const LOCAL_STORAGE_KEY = "favoriteExerciseIds";
 
 const energyFlowApiService = new EnergyFlowApiSevice();
-// energyFlowApiService.getExercisesByCategory("muscles", "abs", 1, 6)
-//   .then((response) => addCardToList(response.data.results));
+energyFlowApiService.getExercisesByCategory("muscles", "abs", 1, 6)
+  .then((response) => addCardToList(response.results));
 
-function addCardToList(results) {
-  console.log(results);
-  const cardElement = results
-    .map(
-      cardData => `
+  function addCardToList(results) {
+    console.log(results);
+    const cardElement = results.map((cardData) => `
       <li class="exercises-card">
         <div class="exercises-card-upper-part">
           <div class="div-rating-or-delete-button">
@@ -23,9 +22,7 @@ function addCardToList(results) {
               <svg class="rating-icon" width="18" height="18">
               <use href="../images/sprite.svg#icon-star"></use></svg>
             </span></div>
-            <button type="button" id="${
-              cardData._id
-            }" class="card-delete card-delte-inactive">
+            <button type="button" id="${cardData._id}" class="card-delete card-delte-inactive">
               <svg class="trash-icon" width="16" height="16">
               <use href="../images/sprite.svg#icon-trash"></use></svg>
             </button>
@@ -40,81 +37,46 @@ function addCardToList(results) {
             <svg class="runing-man-icon" width="24" height="24">
             <use href="../images/sprite.svg#icon-runing-man"></use></svg>
           </div>
-          <p class="card-exercise-name">${capitalizeFirstLetter(
-            cardData.name
-          )}</p>
+          <p class="card-exercise-name">${capitalizeFirstLetter(cardData.name)}</p>
         </div>
         <div class="exercises-card-lower-part">
-          <p>Burned calories: <span class="exercises-card-lower-part-span">${
-            cardData.burnedCalories
-          } / 3 min</span></p>
-          <p>Body part: <span class="exercises-card-lower-part-span">${
-            cardData.bodyPart
-          }</span></p>
-          <p>Target: <span class="exercises-card-lower-part-span">${
-            cardData.target
-          }</span></p>
+          <p>Burned calories: <span class="exercises-card-lower-part-span">${cardData.burnedCalories} / 3 min</span></p>
+          <p>Body part: <span class="exercises-card-lower-part-span">${cardData.bodyPart}</span></p>
+          <p>Target: <span class="exercises-card-lower-part-span">${cardData.target}</span></p>
         </div>
       </li>
-  `
-    )
-    .join('');
+  `).join("");
 
-  exercisesCardList.insertAdjacentHTML('beforeend', cardElement);
+  exercisesCardList.insertAdjacentHTML("beforeend", cardElement);
 
-  let cardExerciseNames = exercisesCardList.querySelectorAll(
-    '.card-exercise-name'
-  );
-  cardExerciseNames.forEach(element => {
-    truncateText(element);
-  });
 }
 
-export function truncateText(element) {
-  let containerWidth = exerciseCardNameWidth.clientWidth;
-  let text = element.textContent.trim();
 
-  if (element.scrollWidth > containerWidth) {
-    let truncatedText = text;
-    let i = text.length;
-
-    while (element.scrollWidth > containerWidth && i > 0) {
-      truncatedText = text.slice(0, i - 1);
-      element.textContent = truncatedText + '...';
-      i--;
-    }
-  }
-}
-
-export function capitalizeFirstLetter(string) {
+function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+
 // --------------delete button-----------------------//
 
-const removeFromFavoritesButtons = document.querySelectorAll('.card-delete');
+const removeFromFavoritesButtons = document.querySelectorAll(".card-delete");
 
 removeFromFavoritesButtons.forEach(button => {
-  button.addEventListener('click', () => {
+  button.addEventListener("click", () =>  {
     const exerciseIdToRemove = this.id;
     const favoriteExerciseIds = getFavoriteExerciseIds();
     const indexToRemove = favoriteExerciseIds.indexOf(exerciseIdToRemove);
 
     if (indexToRemove !== -1) {
       favoriteExerciseIds.splice(indexToRemove, 1);
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY,
-        JSON.stringify(favoriteExerciseIds)
-      );
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(favoriteExerciseIds));
     }
     updateInterfaceAfterRemoval(exerciseIdToRemove);
   });
 });
 
 function updateInterfaceAfterRemoval(exerciseIdToRemove) {
-  const cardToRemove = document.querySelector(
-    `.exercises-card[data-id="${exerciseIdToRemove}"]`
-  ); /////якщо помилка то точно тут
+  const cardToRemove = document.querySelector(`.exercises-card[data-id="${exerciseIdToRemove}"]`);/////якщо помилка то точно тут
 
   if (cardToRemove) {
     cardToRemove.remove();
@@ -123,13 +85,12 @@ function updateInterfaceAfterRemoval(exerciseIdToRemove) {
 
 //--------------------- add to favorites---------------------------
 
-const LOCAL_STORAGE_KEY = 'favoriteExerciseIds';
-const addToFavoritesButtons = document.querySelectorAll(
-  '.exercise-favorite-add-btn'
-);
+
+
+const addToFavoritesButtons = document.querySelectorAll(".exercise-favorite-add-btn");
 
 addToFavoritesButtons.forEach(button => {
-  button.addEventListener('click', async function () {
+  button.addEventListener("click", async function() {
     const exerciseId = this.id;
     console.log(exerciseId);
 
@@ -137,14 +98,12 @@ addToFavoritesButtons.forEach(button => {
 
     if (!favoriteExerciseIds.includes(exerciseId)) {
       favoriteExerciseIds.push(exerciseId);
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY,
-        JSON.stringify(favoriteExerciseIds)
-      );
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(favoriteExerciseIds));
     } else {
-      console.log('this exersice is already in favorites');
+      console.log("this exersice is already in favorites")
     }
-  });
+    
+    });
 });
 
 function getFavoriteExerciseIds() {
