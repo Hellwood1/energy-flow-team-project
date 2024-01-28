4e4b19cf44bc67dbbb348d22fadb74901be7273d
RomaShekel <141637507+RomaShekel@users.noreply.github.com>
2024-01-28 17:29:42 +0200

    вирішення конфлікту
    якщо що то це версія до змін футера і імпорту, я футер міняти все-таки не буду

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 'use strict';
-
 import { closeModal } from './js/modal';
 import { addListeners } from './js/renderCategoriesByFilter';
 import { checkDate } from './js/quote';
-import './js/exercise-card';
 import { initRatings, manageRatingModal } from './js/giveRating';
 import { mobileMenu } from './js/header-modal';
-import { validateEmail, handleFormSubmit } from './js/form-footer';
 
-
+mobileMenu()
 addListeners();
 closeModal();
 checkDate();
 initRatings();
 manageRatingModal();
-mobileMenu();
-validateEmail();
-handleFormSubmit();
+
