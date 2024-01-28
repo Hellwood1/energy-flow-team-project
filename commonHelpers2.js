690880553764d50dbe733c602fef034262ef8a5c
Hellwood1 <140306978+Hellwood1@users.noreply.github.com>
2024-01-28 22:34:03 +0000

    Deploying to gh-pages from @ Hellwood1/energy-flow-team-project@4c176c30864de92dac6f5b119d7f080333dd8286 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/index-d902a5f8.js";const e=document.querySelector(".navigation__links--box");e.lastElementChild.classList.add("active-page");
+//# sourceMappingURL=commonHelpers2.js.map
