5a7ef2d3f5ac43a31d7928ba541184afd5d34543
RomaShekel <141637507+RomaShekel@users.noreply.github.com>
2024-01-28 17:17:32 +0200

    підключення футера

--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ import { addListeners } from './js/renderCategoriesByFilter';
 import { checkDate } from './js/quote';
 import './js/exercise-card';
 import { initRatings, manageRatingModal } from './js/giveRating';
+import { mobileMenu } from './js/header-modal';
+import { validateEmail, handleFormSubmit } from './js/form-footer';
+
 
 addListeners();
 closeModal();
 checkDate();
 initRatings();
 manageRatingModal();
+mobileMenu();
+validateEmail();
+handleFormSubmit();
