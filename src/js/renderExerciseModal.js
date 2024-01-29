5fc4faef1f9daba26e1caec295b956caaa01b689
Vladyslav Lytvyniuk <lytvyniuk.vladyslav@gmail.com>
2024-01-29 20:02:42 +0100

    closebyescape

--- src/js/renderExerciseModal.js
+++ src/js/renderExerciseModal.js
@@ -28,27 +28,12 @@ export const renderExerciseModal = async id => {
 
     closeModealBtn.addEventListener('click', closeExerciseModal);
 
-    document.addEventListener('keydown', e => {
-      if (e.key === 'Escape') {
-        closeExerciseModal();
-      }
-    });
 
     const giveRatingButtons = document.querySelectorAll(
       '.exercise-rating-give-btn'
     );
     giveRatingButtons.forEach(button =>
-      button.addEventListener('click', () => {
-        const ratingBackdrop = document.querySelector(
-          '[data-modal-rating-backdrop]'
-        );
-        const ratingContainer = document.querySelector(
-          '[data-modal-rating-container]'
-        );
-        ratingBackdrop.classList.remove('backdrop-rating-is-hidden');
-        ratingContainer.classList.remove('modal-rating-is-hidden');
-        closeExerciseModal();
-      })
+      button.addEventListener('click', openRatingModal)
     );
   } catch (error) {
     showMessageBadRequest();
@@ -57,11 +42,48 @@ export const renderExerciseModal = async id => {
   function closeExerciseModal() {
     modalBackdrop.classList.add('backdrop-is-hidden');
     modalExercise.classList.add('modal-is-hidden');
+    window.removeEventListener('keydown', closeExerciseCardByEscape);
   }
   function openExerciseModal() {
     modalBackdrop.classList.remove('backdrop-is-hidden');
     modalExercise.classList.remove('modal-is-hidden');
+    window.addEventListener('keydown', closeExerciseCardByEscape);
   }
 
-  
+  function closeExerciseCardByEscape(e) {
+    if (e.key === 'Escape') {
+      closeExerciseModal();
+    }
+  }
+  function closeRatingModalByEscape(e) {
+    if (e.key === 'Escape') {
+      closeRatingModal();
+    }
+  }
+  function closeRatingModal() {
+    const ratingBackdrop = document.querySelector(
+      '[data-modal-rating-backdrop]'
+    );
+    const ratingContainer = document.querySelector(
+      '[data-modal-rating-container]'
+    );
+    ratingBackdrop.classList.add('backdrop-rating-is-hidden');
+    ratingContainer.classList.add('modal-rating-is-hidden');
+    modalBackdrop.classList.remove('backdrop-is-hidden');
+    modalExercise.classList.remove('modal-is-hidden');
+  }
+
+  function openRatingModal(){
+    const ratingBackdrop = document.querySelector(
+      '[data-modal-rating-backdrop]'
+    );
+    const ratingContainer = document.querySelector(
+      '[data-modal-rating-container]'
+    );
+    ratingBackdrop.classList.remove('backdrop-rating-is-hidden');
+    ratingContainer.classList.remove('modal-rating-is-hidden');
+    closeExerciseModal();
+    window.removeEventListener('keydown', closeExerciseCardByEscape);
+    window.addEventListener('keydown', closeRatingModalByEscape);
+  }
 };
