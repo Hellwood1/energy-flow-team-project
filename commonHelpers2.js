a7b6607625e4361dd6f7929ab9ec28c6135f7703
Hellwood1 <140306978+Hellwood1@users.noreply.github.com>
2024-01-30 21:47:47 +0000

    Deploying to gh-pages from @ Hellwood1/energy-flow-team-project@fb52cff04185022eafc0deb4e35d1c7af56ae359 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,26 +1,26 @@
-import{f as l,E as v,i as r}from"./assets/quote-53c236f5.js";import"./assets/vendor-c4bdd18a.js";const u=document.querySelector(".favorites-page"),p=document.querySelector(".header-section");p.classList.add("header-section-favorites");u.classList.add("active-page");l();const g=document.querySelector(".favorites-list"),i=document.querySelector(".favorites-div-without-cards ");document.querySelector(".exercises-card");const c="favoriteExerciseIds",h=new v;function f(e){console.log(e);const s=e.map(t=>`
+import{f as v,E as g,i}from"./assets/quote-53c236f5.js";import"./assets/vendor-c4bdd18a.js";const p=document.querySelector(".favorites-page"),h=document.querySelector(".header-section");h.classList.add("header-section-favorites");p.classList.add("active-page");v();const f=document.querySelector(".favorites-list"),c=document.querySelector(".favorites-div-without-cards ");document.querySelector(".exercises-card");const n="favoriteExerciseIds",m=new g;function a(e){console.log(e);const t=e.map(s=>`
 
-      <li class="exercises-card" id="${t._id}">
+      <li class="exercises-card" id="${s._id}">
         <div class="exercises-card-upper-part">
           <div class="div-rating-or-delete-button">
             <div class="workout-text">WORKOUT</div>
-            <div class="card-rating">${t.rating.toFixed(1)} <span>
+            <div class="card-rating">${s.rating.toFixed(1)} <span>
               <svg class="rating-icon" width="18" height="18">
 
-              <use href="${r}#icon-star"></use></svg>
+              <use href="${i}#icon-star"></use></svg>
 
             </span></div>
-            <button type="button" id="${t._id}" class="card-delete">
+            <button type="button" id="${s._id}" class="card-delete">
               <svg class="trash-icon" width="16" height="16">
 
-              <use href="${r}#icon-trash"></use></svg>
+              <use href="${i}#icon-trash"></use></svg>
 
             </button>
           </div>
           <button type="button" class="card-start">Start <span>
             <svg class="start-icon" width="14" height="14">
 
-            <use href="${r}#icon-right-arrow"></use></svg>
+            <use href="${i}#icon-right-arrow"></use></svg>
 
           </span></button>
         </div>
@@ -28,16 +28,16 @@ import{f as l,E as v,i as r}from"./assets/quote-53c236f5.js";import"./assets/ven
           <div class="exercises-card-midle-part-svg">
             <svg class="runing-man-icon" width="24" height="24">
 
-            <use href="${r}#icon-runing-man"></use></svg>
+            <use href="${i}#icon-runing-man"></use></svg>
 
           </div>
-          <p class="card-exercise-name">${m(t.name)}</p>
+          <p class="card-exercise-name">${x(s.name)}</p>
         </div>
         <div class="exercises-card-lower-part">
-          <p>Burned calories: <span class="exercises-card-lower-part-span">${t.burnedCalories} / 3 min</span></p>
-          <p>Body part: <span class="exercises-card-lower-part-span">${t.bodyPart}</span></p>
-          <p>Target: <span class="exercises-card-lower-part-span">${t.target}</span></p>
+          <p>Burned calories: <span class="exercises-card-lower-part-span">${s.burnedCalories} / 3 min</span></p>
+          <p>Body part: <span class="exercises-card-lower-part-span">${s.bodyPart}</span></p>
+          <p>Target: <span class="exercises-card-lower-part-span">${s.target}</span></p>
         </div>
       </li>
-  `).join("");g.insertAdjacentHTML("beforeend",s),x()}function m(e){return e.charAt(0).toUpperCase()+e.slice(1)}function x(){const e=document.querySelectorAll(".card-delete");console.log(e),e.forEach(s=>E(s))}function E(e){console.log(e),e.addEventListener("click",function(){const s=this.id;console.log(s);let n=a().filter(d=>d!==s);localStorage.setItem(c,JSON.stringify(n)),w(s)})}function w(e){const s=document.querySelector(`.exercises-card[id="${e}"]`);s&&s.remove()}function a(){const e=localStorage.getItem(c);return e?JSON.parse(e):[]}const o=a(),y=async e=>{const s=e.map(t=>h.getExerciseInfoById(t));return Promise.all(s)};o.length!==0?(i.classList.add("favorites-div-without-cards-hidden"),y(o).then(e=>{f(e)}).catch(e=>{console.error("Error fetching data:",e)})):i.classList.remove("favorites-div-without-cards-hidden");
+  `).join("");f.insertAdjacentHTML("beforeend",t),E()}function x(e){return e.charAt(0).toUpperCase()+e.slice(1)}function E(){const e=document.querySelectorAll(".card-delete");console.log(e),e.forEach(t=>w(t))}function w(e){console.log(e),e.addEventListener("click",function(){const t=this.id;console.log(t);let r=d().filter(u=>u!==t);localStorage.setItem(n,JSON.stringify(r)),S(t)})}function S(e){const t=document.querySelector(`.exercises-card[id="${e}"]`);t&&t.remove()}function d(){const e=localStorage.getItem(n);return e?JSON.parse(e):[]}const o=d(),l=async e=>{const t=e.map(s=>m.getExerciseInfoById(s));return Promise.all(t)};o.length!==0?(c.classList.add("favorites-div-without-cards-hidden"),l(o).then(e=>{window.innerWidth<768?(a(e.slice(0,8)),totalFavoritesPages=Math.ceil(e.length/8),renderPageList(totalFavoritesPages,currentPage,currentPage),document.querySelector(".navigation-list-form").addEventListener("submit",L)):a(e)}).catch(e=>{console.error("Error fetching data:",e)})):c.classList.remove("favorites-div-without-cards-hidden");function L(e){e.preventDefault(),currentPage=e.submitter.textContent;const t=JSON.parse(localStorage.getItem("favoriteExerciseIds"));let s=Math.ceil(t.length/8);l(o).then(r=>{a(r.slice((currentPage-1)*8,currentPage*8))}).catch(r=>{console.error("Error fetching data:",r)}),renderPageList(s,currentPage,currentPage)}
 //# sourceMappingURL=commonHelpers2.js.map
