bbc9da267989e1f6ff509213202edf06f1a4bbe3
Gleb Veklenko <glebveklenko59@gmail.com>
2024-01-30 11:03:12 +0200

    quote

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -5,11 +5,3 @@ pageLinks.lastElementChild.classList.add('active-page');
 import { checkDate } from './quote';
 
 checkDate();
-
-// function changeQuote() {
-//   const storedQuote = JSON.parse(localStorage.getItem('quoteObj'));
-//   document.querySelector('.quote-text').innerHTML = storedQuote.quote;
-//   document.querySelector('.quote-author').innerHTML = storedQuote.author;
-// }
-
-// changeQuote();
