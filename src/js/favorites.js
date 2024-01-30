405fb1eab9b5ca190d22da44fab71d3a417f639e
Hellwood1 <samburik123@gmail.com>
2024-01-30 23:14:21 +0100

    fixed mobile menu

--- src/js/favorites.js
+++ src/js/favorites.js
@@ -1,6 +1,7 @@
 import { checkDate } from './quote';
 import { mobileMenu, showCurrentPageFavorites } from './header-modal';
 
+const pageLinks = document.querySelector('.favorites-page');
 const headerEl = document.querySelector('.header-section');
 headerEl.classList.add('header-section-favorites')
 pageLinks.classList.add('active-page');
@@ -9,5 +10,3 @@ pageLinks.classList.add('active-page');
 showCurrentPageFavorites();
 mobileMenu();
 checkDate();
-addListenersToRatingModal();
-
