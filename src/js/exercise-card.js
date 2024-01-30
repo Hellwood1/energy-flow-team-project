7cb23adba7120c23fd6ebc8ffb419e99a4e2b3d3
Hellwood1 <samburik123@gmail.com>
2024-01-30 22:31:09 +0100

    upd.

--- src/js/exercise-card.js
+++ src/js/exercise-card.js
@@ -1,40 +1,21 @@
 
 import EnergyFlowApiSevice from './api-service';
 import imgUrl from '../images/sprite.svg';
-
-import {
-  renderPageList,
-  chooseCurrentPage,
-  changeCurrentPage,
-} from './pagination';
-
-const exercisesCardList = document.querySelector('.favorites-list');
-const favoriteDivWithoutCards = document.querySelector(
-  '.favorites-div-without-cards'
-);
-const listWithoutExercases = document.querySelector(
-  '.favorites-div-without-cards '
-);
-const LOCAL_STORAGE_KEY = 'favoriteExerciseIds';
-
+import { exerciseCardMarkup } from './markup';
+import { renderExerciseModal } from "./renderExerciseModal";
 
 const exercisesCardList = document.querySelector(".favorites-list");
 const listWithoutExercases = document.querySelector(".favorites-div-without-cards ");
-const exercisesCard = document.querySelector(".exercises-card");
+const exercisesCard = document.querySelector(".exercises-card")
 const LOCAL_STORAGE_KEY = "favoriteExerciseIds";
-
 const energyFlowApiService = new EnergyFlowApiSevice();
-let totalFavoritesPages;
-let currentPage = 1;
 // localStorage.clear()
-
 
 
 
   function addCardToList(results) {
     console.log(results)
     const cardElement = results.map((cardData) => `
-
 
       <li class="exercises-card" id="${cardData._id}">
         <div class="exercises-card-upper-part">
@@ -87,12 +68,8 @@ let currentPage = 1;
     )
     .join('');
 
-
-  exercisesCardList.innerHTML = cardElement;
-
-  
+  exercisesCardList.insertAdjacentHTML('beforeend', cardElement);
   deleteButtonEventListener();
-
 }
 
 function capitalizeFirstLetter(string) {
@@ -100,26 +77,6 @@ function capitalizeFirstLetter(string) {
 }
 
 // --------------delete button-----------------------//
-
-
-const exercisesCard = document.querySelector('.exercises-card');
-const removeFromFavoritesButtons = document.querySelectorAll('.card-delete');
-
-// exercisesCard.addEventListener("click", () =>  {
-//   const exerciseIdToRemove = exercisesCard.id;
-//   const favoriteExerciseIds = getFavoriteExerciseIds();
-//   const indexToRemove = favoriteExerciseIds.indexOf(exerciseIdToRemove);
-
-//   if (indexToRemove !== -1) {
-//     favoriteExerciseIds.splice(indexToRemove, 1);
-//     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(favoriteExerciseIds));
-//   }
-//   updateInterfaceAfterRemoval(exerciseIdToRemove);
-// });
-
-removeFromFavoritesButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const exerciseIdToRemove = button.id;
 
 
 function deleteButtonEventListener() {
@@ -134,7 +91,6 @@ function deleteButtonAddEventListener(button) {
   button.addEventListener("click", function() {
   const exerciseIdToRemove = this.id;
   console.log(exerciseIdToRemove);
-
 
   const favoriteExerciseIds = getFavoriteExerciseIds();
   let newFavoriteExerciseIds = favoriteExerciseIds.filter(element => element !== exerciseIdToRemove);
@@ -154,47 +110,7 @@ function updateInterfaceAfterRemoval(exerciseIdToRemove) {
   }
 }
 
-
-// ---------------------start button-------------------------------
-
-const cardStartButton = document.querySelectorAll('.card-start');
-const exerciseModalBackdrop = document.querySelector(
-  '.exercise-modal-backdrop'
-);
-cardStartButton.forEach(button => {
-  button.addEventListener('click', () => {
-    exerciseModalBackdrop.classList.remove('backdrop-is-hidden');
-  });
-});
-
 //--------------------- add to favorites---------------------------
-
-const addToFavoritesButtons = document.querySelectorAll(
-  '.exercise-favorite-add-btn'
-);
-
-addToFavoritesButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const exerciseId = button.id;
-
-    console.log(exerciseId);
-
-    const favoriteExerciseIds = getFavoriteExerciseIds();
-
-    if (!favoriteExerciseIds.includes(exerciseId)) {
-      favoriteExerciseIds.push(exerciseId);
-      localStorage.setItem(
-        LOCAL_STORAGE_KEY,
-        JSON.stringify(favoriteExerciseIds)
-      );
-    } else {
-      console.log('this exersice is already in favorites');
-    }
-  });
-});
-
-
-// --------------------- add to favorites---------------------------
 
 function getFavoriteExerciseIds() {
   const storedIds = localStorage.getItem(LOCAL_STORAGE_KEY);
@@ -204,50 +120,20 @@ function getFavoriteExerciseIds() {
 // -------------------------Завантаження з улюблених-----------------------------------
 const favoriteExerciseIdInLocalStorage = getFavoriteExerciseIds();
 
-const fetchDataForIds = async ids => {
-  const promises = ids.map(id => energyFlowApiService.getExerciseInfoById(id));
+const fetchDataForIds = async (ids) => {
+  const promises = ids.map((id) => energyFlowApiService.getExerciseInfoById(id));
   return Promise.all(promises);
 };
 
 if (favoriteExerciseIdInLocalStorage.length !== 0) {
-  listWithoutExercases.classList.add('favorites-div-without-cards-hidden');
+  listWithoutExercases.classList.add("favorites-div-without-cards-hidden");
   fetchDataForIds(favoriteExerciseIdInLocalStorage)
-    .then(results => {
-      if (window.innerWidth < 768) {
-        addCardToList(results.slice(0, 8));
-        totalFavoritesPages = Math.ceil(results.length / 8);
-        renderPageList(totalFavoritesPages, currentPage, currentPage);
-        document
-          .querySelector('.navigation-list-form')
-          .addEventListener('submit', paginateFavorites);
-      } else {
-        addCardToList(results);
-      }
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
+  .then((results) => { addCardToList(results) })
+  .catch((error) => {
+    console.error('Error fetching data:', error);
+  });
 } else {
-  listWithoutExercases.classList.remove('favorites-div-without-cards-hidden');
+  listWithoutExercases.classList.remove("favorites-div-without-cards-hidden");
 }
 
-
-function paginateFavorites(e) {
-  e.preventDefault();
-
-  currentPage = e.submitter.textContent;
-  const ids = JSON.parse(localStorage.getItem('favoriteExerciseIds'));
-  let total = Math.ceil(ids.length / 8);
-
-  fetchDataForIds(favoriteExerciseIdInLocalStorage)
-    .then(results => {
-      addCardToList(results.slice((currentPage - 1) * 8, currentPage * 8));
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-
-  renderPageList(total, currentPage, currentPage);
-}
-
-//  ---------------------------ПАГІНАЦІЯ----------------------------------
+// ---------------------------ПАГІНАЦІЯ-----------------------------------
